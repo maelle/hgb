d66b5fdfe025e4816e0bdd41bc2d95ad73c53a1d
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-09-03 13:35:53 +0200

    Initial commit

---
+++ R/thing.R
@@ -0,0 +1,12 @@
+#' A function messaging a thing
+#'
+#' @custom Hello I am here
+#'
+#' @return Nothing, print thing
+#' @export
+#'
+#' @examples
+#' thing()
+thing <- function() {
+  message("thing")
+}
